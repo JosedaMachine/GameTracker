97e96a748c8031b2d4113423a26469e6cd2fbaa7
SergioJAlfonso <sergialf@ucm.es>
2023-04-06 11:36:08 +0200

    Inicio de persistencia + cambio de namespace

--- GameTracker/GameTracker/Event.cs
+++ GameTracker/GameTracker/Event.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test {
+namespace Tracker
+{
     enum CaseType
     {
         START,
@@ -60,7 +61,7 @@ namespace Test {
         public EventArgs eventArgs;
     }
 
-    internal class Event {
+    class Event {
         public CaseType caseType_;
         public Case_Union case_;
     }
