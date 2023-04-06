c4c4f3d2d2fe0bdad3f10b566db7b13606fbbcd7
SergioJAlfonso <sergialf@ucm.es>
2023-04-06 11:40:14 +0200

    Namespace Gametracker

--- GameTracker/GameTracker/IPersistence.cs
+++ GameTracker/GameTracker/IPersistence.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.GameTracker.GameTracker.GameTracker
+namespace GameTracker
 {
     interface IPersistence
     {
