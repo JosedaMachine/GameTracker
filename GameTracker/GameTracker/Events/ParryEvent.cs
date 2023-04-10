b1f55133225ee55dd30e14fe448bce1c603e1ee1
SergioJAlfonso <sergialf@ucm.es>
2023-04-10 19:53:59 +0200

    sistema de carpetas + inicio ParryEvent

---
+++ GameTracker/GameTracker/Events/ParryEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace GameTracker
+{
+    internal class ParryEvent : TrackerEvent
+    {
+        public ParryEvent(CommonContent common) : base(common){
+            eventType_ = "Parry";
+        }
+
+        public override string toCSV()
+        {
+            //Base information
+            string format = base.toCSV();
+
+            return format + "\n";
+        }
+
+        public override string toJSON()
+        {
+            //Base information
+            string format = base.toJSON();
+
+            //extract collection data
+            var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(format);
+
+            //Add data
+            data["Level"] = "Satanic Alberto";
+
+            // Serialize collection with new data
+            string newCollection = JsonConvert.SerializeObject(data, new JsonSerializerSettings { Formatting = Formatting.Indented });
+
+            //Close file
+            newCollection += "\n]";
+
+            return newCollection;
+        }
+
+    }
+}
