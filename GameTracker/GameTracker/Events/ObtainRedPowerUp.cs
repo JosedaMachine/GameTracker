f1b149c7c759bba305baef25b64844dd6a3bbd1f
Ángel López <62568478+angelo066@users.noreply.github.com>
2023-04-10 19:52:02 +0200

    Evento de Red Power Up

---
+++ GameTracker/GameTracker/Events/ObtainRedPowerUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker
+{
+    internal class ObtainRedPowerUp : TrackerEvent
+    {
+        public ObtainRedPowerUp(CommonContent common) : base(common)
+        {
+            eventType_ = "RedPowerUpObtained";
+        }
+
+        public override string toCSV()
+        {
+            //Base information
+            string format = base.toCSV();
+
+            return format + "\n";
+        }
+
+        public override string toJSON()
+        {
+            //Base information
+            string format = base.toJSON();
+
+            //extract collection data
+            var data = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(format);
+
+            // Serialize collection with new data
+            string newCollection = System.Text.Json.JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+
+            //Close file
+            newCollection += "\n]";
+
+            return newCollection;
+        }
+
+    }
+}
